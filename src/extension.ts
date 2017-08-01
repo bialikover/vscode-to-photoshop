dddb084939d8ba6007bc88992ecaa8a8f077f420
Adan G Galvan Gonzalez <bialikoer@gmail.com>
2017-07-31 23:05:09 -0700

    add osascript and basic functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,8 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as vscode from 'vscode';
+import * as osascript from 'node-osascript';
+
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -14,11 +12,35 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.evaluateScript', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('Evaluating Extend Script');
+
+        let editor = vscode.window.activeTextEditor;
+
+        if (!editor) {
+            return;
+        }
+        let selection = editor.selection;
+        let text = selection.isEmpty ? editor.document.getText() : editor.document.getText(selection);
+        let script = `tell application "Adobe Photoshop CC 2017.1.1" to do javascript "(${text})"`;
+        let outputChannel = vscode.window.createOutputChannel('Extend Script');
+        //show the output.
+        outputChannel.show()
+        osascript.execute(script, (err, res, raw)=>{
+            if(err){
+                showOutput(err);
+                return;
+            }
+            showOutput(res);
+        })
+
+        function showOutput(message:string){
+            outputChannel.append(`${new Date().toISOString()} ${message}`);
+
+          }
     });
 
     context.subscriptions.push(disposable);
